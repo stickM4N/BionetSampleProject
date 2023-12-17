983fa18e97b7234d62b8e8a246ce7cae12b8468a
stickm4n <jcgalindo.jcgh@google.com>
2023-12-17 03:00:57 +0100

    enhanced base request handler validation

--- application/request_handlers/core/base_request_handler.py
+++ application/request_handlers/core/base_request_handler.py
@@ -1,12 +1,11 @@
 from abc import ABC, abstractmethod
-from types import NoneType
 from typing import Callable
 
 from application.requests.core.base_request import BaseRequest
 
 
 class BaseRequestHandler(ABC):
-    _request_type: type[BaseRequest, NoneType] = None
+    _request_type: type[BaseRequest] = None
 
     def __init__(self):
         assert self._request_type is not None, '_request_type field is required'
@@ -21,12 +20,17 @@ class BaseRequestHandler(ABC):
         return self._request_type.result_type.fget(self._request_type)
 
     @staticmethod
-    def assert_return(handle: Callable) -> Callable:
+    def validate_handle(handle: Callable) -> Callable:
         async def wrapper(self, request: BaseRequest):
-            assert request.result_type == self.result_type, \
-                f'Handle return value is not an instance of {self.result_type.__name__}'
+            assert isinstance(request, self.request_type), \
+                f'request is not an instance of {self.request_type.__name__}'
 
-            return await handle(self, request)
+            result = await handle(self, request)
+
+            assert isinstance(result, self.result_type), \
+                f'result is not an instance of {self.result_type.__name__}'
+
+            return result
 
         return wrapper
 
